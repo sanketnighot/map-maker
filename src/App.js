f4d601b359c4bf9a98497d15e4becefd47bcb3b7
surajyadav24 <84555072+surajyadav24@users.noreply.github.com>
2022-01-24 15:44:59 +0000

    map changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 // import './App.css';
 import Map from './Components/Map';
-
+import background from './Components/map.png'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{
+      backgroundImage: `url(${background})`,
+      overflowX: 'scroll', whiteSpace:'nowrap',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize:'cover',
+     height:'100%',
+      backgroundPosition:'center',
+      backgroundAttachment: 'local',
+      // width: '100vw',
+      height: '300vh'
+    }}>
       <Map/>
     </div>
   );
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
-import { Stage, Layer, Rect} from 'react-konva';
-import MapData from './mapData.json'
-
+import React, { useState } from "react";
+import { Stage, Layer, Rect } from "react-konva";
+import MapData from "./mapData.json";
+import background from "./map.png"
 const Map = () => {
-const [eid, setId] = useState(0)
+  const [eid, setId] = useState(0);
 
-const [stage, setStage] = useState({
+  const [stage, setStage] = useState({
     scale: 1,
-    x: 0,
-    y: 0
+    x: 400,
+    y: 700,
   });
 
   const handleWheel = (e) => {
@@ -19,7 +19,7 @@ const [stage, setStage] = useState({
     const oldScale = stage.scaleX();
     const mousePointTo = {
       x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
-      y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
+      y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale,
     };
 
     const newScale = e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy;
@@ -27,44 +27,57 @@ const [stage, setStage] = useState({
     setStage({
       scale: newScale,
       x: (stage.getPointerPosition().x / newScale - mousePointTo.x) * newScale,
-      y: (stage.getPointerPosition().y / newScale - mousePointTo.y) * newScale
+      y: (stage.getPointerPosition().y / newScale - mousePointTo.y) * newScale,
     });
   };
 
-  	const width = 125;
-	const height = 125;
-	const Map = MapData.map(
-		(data) => {
-		
-			return(
-				<Rect id={data.id} x={(data.x * height)+5} y={(data.y * width)+5} width={125} height={125} fill={(data.type === "basic" ? "lightBlue" : "lightPink")} shadowBlur={(eid === data.id) ? 5 : 0} stroke={(eid === data.id) ? 'green' : 'black'} zIndex={(eid === data.id) ? 9 : 0} strokeWidth={(eid === data.id) ? 7 : 5} onClick={(e)=>{setId(data.id);}} onTap={(e)=>{setId(data.id)}}/>
-			);
-		}
-);
-
+  const width = 50;
+  const height = 50;
+  const Map = MapData.map((data) => {
     return (
-        <>
-            <Stage width={window.innerWidth}
-                    height={window.innerHeight*(0.8)}
-                    onWheel={handleWheel}
-                    scaleX={stage.scale}
-                    scaleY={stage.scale}
-                    x={stage.x}
-                    y={stage.y} 
-                    draggable={true}
-                    style={{backgroundColor:"grey"}}>
-                <Layer>
-				{Map}
-                </Layer>
-            </Stage>
-            <div style={{marginLeft:"5%"}}>
-                Data: 
-                {/* Name: {data[eid].name} */}
-            </div>
+      <Rect
+        id={data.id}
+        x={data.x * height + 5}
+        y={data.y * width + 5}
+        width={50}
+        height={50}
+        fill={data.type === "basic" ? "lightBlue" : "lightPink"}
+        shadowBlur={eid === data.id ? 5 : 0}
+        stroke={eid === data.id ? "green" : "black"}
+        zIndex={eid === data.id ? 9 : 0}
+        strokeWidth={eid === data.id ? 7 : 5}
+        onClick={(e) => {
+          setId(data.id);
+        }}
+        onTap={(e) => {
+          setId(data.id);
+        }}
+      />
+    );
+  });
 
-            
-        </>
-    )
-}
+  return (
+    <>
+      <Stage
+        width={window.innerWidth}
+        height={1300}
+        // onWheel={handleWheel}
+        // scaleX={stage.scale}
+        // scaleY={stage.scale}
+        x={stage.x}
+        y={stage.y}
+        draggable={false}
+      >
+        <Layer>{Map}</Layer>
+      </Stage>
+      <div style={{ 
 
-export default Map
+    }}>
+        Data:
+        {/* Name: {data[eid].name} */}
+      </div>
+    </>
+  );
+};
+
+export default Map;
