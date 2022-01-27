182a96247f6e28aaca2ae4a4f0bcb841090112f3
sanketnighot <sanketnighot25@gmail.com>
2022-01-27 14:37:49 +0530

    -

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Stage, Layer, Rect} from 'react-konva';
+import {Stage, Layer, Rect} from 'react-konva';
 import MapData from './mapData.json'
 
 const Map = () => {
@@ -7,13 +7,12 @@ const [eid, setId] = useState(0)
 
 const [stage, setStage] = useState({
     scale: 1,
-    x: 0,
-    y: 0
+		x: 0,
+		y: 0
   });
 
   const handleWheel = (e) => {
     e.evt.preventDefault();
-
     const scaleBy = 1.02;
     const stage = e.target.getStage();
     const oldScale = stage.scaleX();
@@ -31,13 +30,12 @@ const [stage, setStage] = useState({
     });
   };
 
-  	const width = 125;
-	const height = 125;
+  	const width = 15;
 	const Map = MapData.map(
 		(data) => {
 		
 			return(
-				<Rect id={data.id} x={(data.x * height)+5} y={(data.y * width)+5} width={125} height={125} fill={(data.type === "basic" ? "lightBlue" : "lightPink")} shadowBlur={(eid === data.id) ? 15 : 0} stroke={(eid === data.id) ? 'green' : 'black'} zIndex={(eid === data.id) ? 19 : 0} strokeWidth={(eid === data.id) ? 7 : 5} onClick={(e)=>{setId(data.id);}} onTap={(e)=>{setId(data.id)}}/>
+				<Rect id={data.id} x={(data.x * width)+5} y={(data.y * width)+5} width={width} height={width} fill={(data.type === "basic" ? "lightBlue" : "lightPink")} shadowBlur={(eid === data.id) ? 2 : 0} stroke={(eid === data.id) ? 'green' : 'black'} zIndex={(eid === data.id) ? 5*5 : 0} strokeWidth={(eid === data.id) ? 0.75 : 0.2} onClick={()=>{setId(data.id);}} onTap={()=>{setId(data.id)}}/>
 			);
 		}
 );
@@ -52,17 +50,22 @@ const [stage, setStage] = useState({
                     x={stage.x}
                     y={stage.y} 
                     draggable={true}
-                    style={{backgroundColor:"grey"}}>
+                    style={{backgroundColor:"#BBBBBB"}}>
                 <Layer>
 				{Map}
                 </Layer>
             </Stage>
-            <div style={{marginLeft:"5%"}}> 
-                <h1>Name: {MapData[eid - 1].owner}</h1>
+			<center>
+            <div style={{marginLeft:"5%", display: "inlineBlock"}}> 
+			{(eid !== 0) ? 
+				<>
+                <h1>Name: {MapData[eid-1].owner}</h1>
 				<h2>Location: {MapData[eid - 1].x}, {MapData[eid - 1].y}</h2>
-				<h2>Land Type: {MapData[eid - 1].type}</h2>
+				<h2>Land Type: {MapData[eid - 1].type} </h2> 
+				</>:
+			 <h1>Click/Tap the Tile to get Data</h1>}
             </div>
-
+			</center>
             
         </>
     )
